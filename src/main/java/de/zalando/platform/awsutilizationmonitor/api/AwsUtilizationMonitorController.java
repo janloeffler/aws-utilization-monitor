360fd9ce50b9b6ff57be36ea8f8f278276dbb2ad
Jan Löffler <mail@jlsoft.de>
2015-02-09 16:03:53 +0100

    fixed REST params

--- src/main/java/de/zalando/platform/awsutilizationmonitor/api/AwsUtilizationMonitorController.java
+++ src/main/java/de/zalando/platform/awsutilizationmonitor/api/AwsUtilizationMonitorController.java
@@ -33,10 +33,10 @@ final class AwsUtilizationMonitorController {
 		return collector.getStats().getAccounts();
 	}
 
-	@RequestMapping("/accounts/{accountName}")
+	@RequestMapping("/accounts/{accountName}/")
 	@ResponseBody
 	AwsResource[] accounts(@PathVariable String accountName) {
-		LOG.info("called /accounts/" + accountName);
+		LOG.info("called /accounts/" + accountName + "/");
 
 		AwsResource[] results = collector.getStats().getResourcesByAccount(accountName);
 
@@ -55,16 +55,11 @@ final class AwsUtilizationMonitorController {
 		return collector.getStats().getApps();
 	}
 
-	@RequestMapping("/apps/{appName}")
+	@RequestMapping("/apps/{appName}/")
 	@ResponseBody
 	AwsResource[] apps(@PathVariable String appName) {
-		try {
-			appName = URLDecoder.decode(appName, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			LOG.error("Cannot decode \"" + appName + "\": " + e.getMessage());
-		}
-
-		LOG.info("called /apps/" + appName);
+		appName = decodeParam(appName);
+		LOG.info("called /apps/" + appName + "/");
 
 		AwsResource[] res = collector.getStats().getAppInstances(appName);
 
@@ -83,6 +78,16 @@ final class AwsUtilizationMonitorController {
 		collector.clearCache();
 
 		return "Cache empty";
+	}
+
+	String decodeParam(String param) {
+		try {
+			return URLDecoder.decode(param, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			LOG.error("Cannot decode \"" + param + "\": " + e.getMessage());
+		}
+
+		return param;
 	}
 
 	String encodeParam(String param) {
@@ -116,27 +121,27 @@ final class AwsUtilizationMonitorController {
 		LOG.info("called /");
 
 		return "<html><header><style>p, li, ul, a { font-family:'Courier New', Arial; }</style></header><body><h1>AWS Utilization Statistics</h1><p><ul>"
-				+ "<li><a href=/apps/>/apps/</a> List EC2 based apps</li>"
-				+ "<li><a href=/apps/app_1/>/apps/{app_name}/</a> Show EC2 based apps with name \"app_1\"</li>"
-				+ "<li><a href=/instancetypes/>/instancetypes/</a> List used EC2 instance types</li>"
-				+ "<li><a href=/instancetypes/"
-				+ encodeParam("t2.micro")
-				+ "/>/instancetypes/{instance_type}/</a> Show EC2 based apps with instance type \"t2.micro\"</li>"
-				+ "<li><a href=/resources/>/resources/</a> List resources</li>"
-				+ "<li><a href=/resources/app_1/>/resources/{resource_name}/</a> Show resources with name \"app_1\"</li>"
-				+ "<li><a href=/keys/>/keys/</a> List keys</li>"
-				+ "<li><a href=/keys/PublicDnsName/>/keys/{key_name}/</a> Show resources that contain a value with the key \"PublicDnsName\"</li>"
-				+ "<li><a href=/keys/Team/>/keys/Team/</a> List team names if \"team\" tag was specified</li>"
-				+ "<li><a href=/accounts/>/accounts/</a> List accounts</li>"
-				+ "<li><a href=/accounts/123456789012/>/accounts/{account_name}/</a> Show resources used by account with name \"123456789012\"</li>"
-				+ "<li><a href=/regions/>/regions/</a> List regions</li>"
-				+ "<li><a href=/regions/EU_WEST_1/>/regions/{region_name}/</a> Show resources used by region with name \"EU_WEST_1\"</li>"
-				+ "<li><a href=/search/banana/>/search/{search_pattern}/</a> Show app with name \"banana\"</li>"
-				+ "<li><a href=/values/Team/Platform/>/values/{key_name}/{value_pattern}/</a> Show resources that contain a value with the key \"Team\" and the pattern \"Platform\"</li>"
-				+ "<li><a href=/statistics/>/statistics/</a> Show statistics about resource usage</li>"
-				+ "<li><a href=/summary/>/summary/</a> Show summary KPIs only about resource usage</li>"
-				+ "<li><a href=/test/>/test/</a> Generate test data</li>" + "<li><a href=/test/30>/test/{maxItems}</a> Generate test data with 30 items</li>"
-				+ "<li><a href=/clear/>/clear/</a> Clear data cache</li>" + "<li><a href=/health/>/health/</a> Show health</li>" + "</ul></p></body></html>";
+		+ "<li><a href=/apps/>/apps/</a> List EC2 based apps</li>"
+		+ "<li><a href=/apps/app_1/>/apps/{app_name}/</a> Show EC2 based apps with name \"app_1\"</li>"
+		+ "<li><a href=/instancetypes/>/instancetypes/</a> List used EC2 instance types</li>"
+		+ "<li><a href=/instancetypes/"
+		+ encodeParam("t2.micro")
+		+ "/>/instancetypes/{instance_type}/</a> Show EC2 based apps with instance type \"t2.micro\"</li>"
+		+ "<li><a href=/resources/>/resources/</a> List resources</li>"
+		+ "<li><a href=/resources/app_1/>/resources/{resource_name}/</a> Show resources with name \"app_1\"</li>"
+		+ "<li><a href=/keys/>/keys/</a> List keys</li>"
+		+ "<li><a href=/keys/PublicDnsName/>/keys/{key_name}/</a> Show resources that contain a value with the key \"PublicDnsName\"</li>"
+		+ "<li><a href=/keys/Team/>/keys/Team/</a> List team names if \"team\" tag was specified</li>"
+		+ "<li><a href=/accounts/>/accounts/</a> List accounts</li>"
+		+ "<li><a href=/accounts/123456789012/>/accounts/{account_name}/</a> Show resources used by account with name \"123456789012\"</li>"
+		+ "<li><a href=/regions/>/regions/</a> List regions</li>"
+		+ "<li><a href=/regions/EU_WEST_1/>/regions/{region_name}/</a> Show resources used by region with name \"EU_WEST_1\"</li>"
+		+ "<li><a href=/search/banana/>/search/{search_pattern}/</a> Show app with name \"banana\"</li>"
+		+ "<li><a href=/values/Team/Platform/>/values/{key_name}/{value_pattern}/</a> Show resources that contain a value with the key \"Team\" and the pattern \"Platform\"</li>"
+		+ "<li><a href=/statistics/>/statistics/</a> Show statistics about resource usage</li>"
+		+ "<li><a href=/summary/>/summary/</a> Show summary KPIs only about resource usage</li>"
+		+ "<li><a href=/test/>/test/</a> Generate test data</li>" + "<li><a href=/test/30>/test/{maxItems}</a> Generate test data with 30 items</li>"
+		+ "<li><a href=/clear/>/clear/</a> Clear data cache</li>" + "<li><a href=/health/>/health/</a> Show health</li>" + "</ul></p></body></html>";
 	}
 
 	@RequestMapping("/instancetypes/")
@@ -147,10 +152,10 @@ final class AwsUtilizationMonitorController {
 		return collector.getStats().getUsedEC2InstanceTypes();
 	}
 
-	@RequestMapping("/instancetypes/{instanceType}")
+	@RequestMapping("/instancetypes/{instanceType}/")
 	@ResponseBody
 	AwsResource[] instancetypes(@PathVariable String instanceType) {
-		LOG.info("called /instancetypes/" + instanceType);
+		LOG.info("called /instancetypes/" + instanceType + "/");
 
 		AwsResource[] res = collector.getStats().getResourcesByEC2InstanceType(instanceType);
 
@@ -169,10 +174,10 @@ final class AwsUtilizationMonitorController {
 		return collector.getStats().getKeys();
 	}
 
-	@RequestMapping("/keys/{keyName}")
+	@RequestMapping("/keys/{keyName}/")
 	@ResponseBody
 	Object[] keys(@PathVariable String keyName) {
-		LOG.info("called /keys/" + keyName);
+		LOG.info("called /keys/" + keyName + "/");
 
 		collector.getStats().getValues(keyName);
 		Object[] results = collector.getStats().getValues(keyName);
@@ -192,10 +197,10 @@ final class AwsUtilizationMonitorController {
 		return collector.getStats().getRegions();
 	}
 
-	@RequestMapping("/regions/{region}")
+	@RequestMapping("/regions/{region}/")
 	@ResponseBody
 	AwsResource[] regions(@PathVariable String region) {
-		LOG.info("called /regions/" + region);
+		LOG.info("called /regions/" + region + "/");
 
 		AwsResource[] results = collector.getStats().getResourcesByRegion(Regions.valueOf(region));
 
@@ -214,10 +219,10 @@ final class AwsUtilizationMonitorController {
 		return collector.getStats();
 	}
 
-	@RequestMapping("/resources/{resourceName}")
+	@RequestMapping("/resources/{resourceName}/")
 	@ResponseBody
 	AwsResource resources(@PathVariable String resourceName) {
-		LOG.info("called /resources/" + resourceName);
+		LOG.info("called /resources/" + resourceName + "/");
 
 		AwsResource res = collector.getStats().getResource(resourceName);
 
@@ -228,16 +233,11 @@ final class AwsUtilizationMonitorController {
 		return res;
 	}
 
-	@RequestMapping("/search/{searchPattern}")
+	@RequestMapping("/search/{searchPattern}/")
 	@ResponseBody
 	AwsResource[] search(@PathVariable String searchPattern) {
-		try {
-			searchPattern = URLDecoder.decode(searchPattern, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			LOG.error("Cannot decode \"" + searchPattern + "\": " + e.getMessage());
-		}
-
-		LOG.info("called /search/" + searchPattern);
+		searchPattern = decodeParam(searchPattern);
+		LOG.info("called /search/" + searchPattern + "/");
 
 		AwsResource[] results = collector.getStats().searchResources(searchPattern);
 
@@ -251,7 +251,7 @@ final class AwsUtilizationMonitorController {
 	@RequestMapping("/statistics/")
 	@ResponseBody
 	String statistics() {
-		LOG.info("called /statistics");
+		LOG.info("called /statistics/");
 
 		AwsStats stats = collector.getStats();
 		StringBuilder s = new StringBuilder();
@@ -263,6 +263,7 @@ final class AwsUtilizationMonitorController {
 		String[] teams = stats.getTeams();
 		String[] apps = stats.getApps();
 		String[] instanceTypes = stats.getUsedEC2InstanceTypes();
+		AwsResource[] ec2instances = stats.getResources(AwsResourceType.EC2);
 
 		s.append("<html><header><style>p, li, ul, a { font-family:'Courier New', Arial; }</style></header><body><h1>AWS Utilization Statistics</h1><p>"
 				+ "<a href=/>Back to overview</a><ul>" + "<li><a href=/resources/>" + resources.length + "</a> resources used</li>");
@@ -272,7 +273,7 @@ final class AwsUtilizationMonitorController {
 
 		for (Regions region : regions) {
 			int amount = stats.getResourcesByRegion(region).length;
-			s.append("<li><a href=/regions/" + region + ">" + amount + "</a> resources in \"" + region + "\"</li>");
+			s.append("<li><a href=/regions/" + region + "/>" + amount + "</a> resources in \"" + region + "\"</li>");
 		}
 
 		s.append("</ul>");
@@ -282,7 +283,7 @@ final class AwsUtilizationMonitorController {
 
 		for (AwsResourceType resourceType : resourceTypes) {
 			int amount = stats.getResources(resourceType).length;
-			s.append("<li><a href=/types/" + resourceType + ">" + amount + "</a> " + resourceType + "</li>");
+			s.append("<li><a href=/types/" + resourceType + "/>" + amount + "</a> " + resourceType + "</li>");
 		}
 
 		s.append("</ul>");
@@ -292,7 +293,7 @@ final class AwsUtilizationMonitorController {
 
 		for (String accountName : accounts) {
 			int amount = stats.getResourcesByAccount(accountName).length;
-			s.append("<li><a href=/accounts/" + accountName + ">" + amount + "</a> resources by \"" + accountName + "\"</li>");
+			s.append("<li><a href=/accounts/" + accountName + "/>" + amount + "</a> resources by \"" + accountName + "\"</li>");
 		}
 
 		s.append("</ul>");
@@ -302,7 +303,7 @@ final class AwsUtilizationMonitorController {
 
 		for (String teamName : teams) {
 			int amount = stats.searchResources("Team", teamName).length;
-			s.append("<li><a href=/values/Team/" + encodeParam(teamName) + ">" + amount + "</a> resources by \"" + teamName + "\"</li>");
+			s.append("<li><a href=/values/Team/" + encodeParam(teamName) + "/>" + amount + "</a> resources by \"" + teamName + "\"</li>");
 		}
 
 		s.append("</ul>");
@@ -312,17 +313,18 @@ final class AwsUtilizationMonitorController {
 
 		for (String appName : apps) {
 			int amount = stats.getAppInstances(appName).length;
-			s.append("<li><a href=/apps/" + encodeParam(appName) + ">" + amount + "</a> instances of \"" + appName + "\"</li>");
+			s.append("<li><a href=/apps/" + encodeParam(appName) + "/>" + amount + "</a> instances of \"" + appName + "\"</li>");
 		}
 
 		s.append("</ul>");
 
 		/* EC2 instance types */
-		s.append("<li><a href=/instancetypes/>" + instanceTypes.length + "</a> used EC2 instance types</li>" + "<ul>");
+		s.append("<li><a href=/instancetypes/>" + instanceTypes.length + "</a> used EC2 instance types by <a href=/types/EC2/>" + ec2instances.length
+				+ "</a> EC2 instances</li>" + "<ul>");
 
 		for (String instanceType : instanceTypes) {
 			int amount = stats.getResourcesByEC2InstanceType(instanceType).length;
-			s.append("<li><a href=/instancetypes/" + encodeParam(instanceType) + ">" + amount + "</a> instances with \"" + instanceType + "\"</li>");
+			s.append("<li><a href=/instancetypes/" + encodeParam(instanceType) + "/>" + amount + "</a> instances with \"" + instanceType + "\"</li>");
 		}
 
 		s.append("</ul>");
@@ -350,10 +352,10 @@ final class AwsUtilizationMonitorController {
 		return collector.getStats();
 	}
 
-	@RequestMapping("/test/{maxItems}")
+	@RequestMapping("/test/{maxItems}/")
 	@ResponseBody
 	AwsStats test(@PathVariable int maxItems) {
-		LOG.info("called /test/" + maxItems);
+		LOG.info("called /test/" + maxItems + "/");
 
 		collector.generateSampleData(maxItems);
 
@@ -368,10 +370,10 @@ final class AwsUtilizationMonitorController {
 		return collector.getStats().getUsedResourceTypes();
 	}
 
-	@RequestMapping("/types/{resourceType}")
+	@RequestMapping("/types/{resourceType}/")
 	@ResponseBody
 	AwsResource[] types(@PathVariable String resourceType) {
-		LOG.info("called /types/" + resourceType);
+		LOG.info("called /types/" + resourceType + "/");
 
 		AwsResource[] results = collector.getStats().getResources(AwsResourceType.valueOf(resourceType));
 
@@ -382,16 +384,11 @@ final class AwsUtilizationMonitorController {
 		return results;
 	}
 
-	@RequestMapping("/values/{key}/{value}")
+	@RequestMapping("/values/{key}/{value}/")
 	@ResponseBody
 	AwsResource[] values(@PathVariable String key, @PathVariable String value) {
-		try {
-			value = URLDecoder.decode(value, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			LOG.error("Cannot decode \"" + value + "\": " + e.getMessage());
-		}
-
-		LOG.info("called /values/" + key + "/" + value);
+		value = decodeParam(value);
+		LOG.info("called /values/" + key + "/" + value + "/");
 
 		AwsResource[] results = collector.getStats().searchResources(key, value);
 
