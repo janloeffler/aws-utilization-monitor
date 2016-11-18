86967d9176070e4ad9d86c1c94b87cc614af423d
Jan Löffler <mail@jlsoft.de>
2016-11-18 16:08:05 +0100

    changed naming

--- src/main/java/de/zalando/platform/awsutilizationmonitor/stats/AwsStatsSummary.java
+++ src/main/java/de/zalando/platform/awsutilizationmonitor/stats/AwsStatsSummary.java
@@ -14,9 +14,9 @@ public class AwsStatsSummary {
 	public static String readableFileSize(long size) {
 		if (size <= 0)
 			return "0";
-		final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
-		int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
-		return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+		final String[] descriptions = new String[] { "B", "kB", "MB", "GB", "TB" };
+		int sizeType = (int) (Math.log10(size) / Math.log10(1024));
+		return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, sizeType)) + " " + descriptions[sizeType];
 	}
 
 	public static String readableLong(long number) {
